bcb68b33e44542e56ff367d8720ad07bd845f57b
egatakebede <egatakebede7@gmail.com>
2026-02-15 19:21:07 +0300

    updaate

--- jobAppApi/cmd/server/main.go
+++ jobAppApi/cmd/server/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"jobAppApi/internal/repository"
+	"log"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	db, err := repository.InitDB()
+	if err != nil {
+		log.Fatal("Failed to initialize database: %v", err)
+	}
+
 	r := gin.Default()
 
 	r.Run(":9080")
